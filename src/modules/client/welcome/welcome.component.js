12ac6462459a75b3880769e53c332fee34feb7dc
rishangit <n.rishee@gmail.com>
2020-05-09 02:43:43 +0530

    fill color

--- src/modules/client/welcome/welcome.component.js
+++ src/modules/client/welcome/welcome.component.js
@@ -5,11 +5,24 @@ const WelcomeComponent = props => {
     <div className={'theme-welcome-wrp'}>
       <div className={'upper-wrp'}>
         <div>Welcome 2</div>
+        <section className={'theme-wave2'}>
+        <svg
+          id="Layer_1"
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1416.99 174.01"
+        >
+          <path
+            fill="#ffffff"
+            d="M0,280.8S283.66,59,608.94,163.56s437.93,150.57,808,10.34V309.54H0V280.8Z"
+            transform="translate(0 -135.53)"
+          />
+        </svg>
+      </section>
       </div>
 
-      <div className={'lower-wrp'}>
-        <div className={'theme-wave'}></div>
-      </div>
+
+      <div className={'lower-wrp'}></div>
     </div>
   );
 };
