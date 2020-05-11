34002bdffce910e72f097f8d543b7c7e15f19ada
rishangit <n.rishee@gmail.com>
2020-05-12 03:27:41 +0530

    add png for the logo

--- src/modules/client/welcome/welcome.component.js
+++ src/modules/client/welcome/welcome.component.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { ArrowDownward, ArrowForwardIos } from '@material-ui/icons';
 // import imglogo from '../../../scss/themes/theme_four/images/logfull.png';
-import 'animate.css/animate.css';
+//import 'animate.css/animate.css';
 
 const WelcomeComponent = props => {
   return (
@@ -41,11 +41,11 @@ const WelcomeComponent = props => {
         <div className={'message-wrp'}>
           <div className={'message'}>
             <p>Click Here to start</p>
-            <ArrowDownward
+            {/* <ArrowDownward
               // className={
               //   'animate__animated animate__bounce animate__slow animate__infinite'
               // }
-            />
+            /> */}
           </div>
         </div>
         <div className={'button-wrp'}>
