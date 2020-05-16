3d3c71283249411ae77affefbc2329bd215f4974
rishangit <n.rishee@gmail.com>
2020-05-16 11:23:57 +0530

    home page svg with common colors

--- src/modules/client/home/home.component.js
+++ src/modules/client/home/home.component.js
@@ -19,6 +19,9 @@ const HomeComponent = props => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
+    autoplay:true,
+    autoplaySpeed:3000,
+    dots:true,
     afterChange: function (index) {
       dispatch(
         commonMenuBarButtonChange({
